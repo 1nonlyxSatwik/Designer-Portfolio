dcc4a6f668b32a4663d3939e3786c457947bf5d0
Satwik mani Triparthi <manisunita30@gmail.com>
2026-04-25 23:03:03 +0530

    Auto commit 2026-04-25_23:03:03

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,9 +1,11 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttpModule from "pino-http";
 import router from "./routes/index.js";
 import healthRouter from "./routes/health.js";
 import { logger } from "./lib/logger.js";
+
+const pinoHttp = (pinoHttpModule.default || (pinoHttpModule as any).pinoHttp || pinoHttpModule) as any;
 
 const app: Express = express();
 
